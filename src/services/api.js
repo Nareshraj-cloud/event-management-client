63b8f0de57b3e4a7c2583e62b3927e17fe3593fe
Nareshraj <axenaresh96@gmail.com>
2026-08-10 02:22:22 +0530

    Update API base URL to deployed backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://event-management-server-70p2.onrender.com/api",
 });
 
 // Attach JWT token to every request if user is logged in
